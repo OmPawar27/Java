24528d0a4ab3c83df52662d8f519b30779e0ea8c
Om Pawar <ompawar4242@gmail.com>
2024-02-28 14:27:44 +0530

    inital commit

--- String/Program12.java
+++ String/Program12.java
@@ -1,7 +1,3 @@
-
-// import java.util.ArrayList;
-import java.util.*;
-
 public class Program12 {
     public static void main(String[] args) {
         String str = "ZZADOBECODEBANC";
@@ -29,46 +25,5 @@ public class Program12 {
             }
         }
         System.out.println(result);
-
-        /*
-         * ArrayList<Character> al = new ArrayList<>();
-         * ArrayList<Character> alresult = new ArrayList<>();
-         * ArrayList<Character> alss = new ArrayList<>();
-         * 
-         * int count = 0;
-         * int index = -1;
-         * boolean flag = false;
-         * 
-         * for (int i = 0; i < str.length(); i++) {
-         * for (int j = 0; j < ss.length(); j++) {
-         * if (ss.charAt(j) == str.charAt(i)) {
-         * index = i;
-         * flag = true;
-         * break;
-         * }
-         * }
-         * if (flag)
-         * break;
-         * }
-         * int min = Integer.MAX_VALUE;
-         * 
-         * for (int i = index; i < str.length(); i++) {
-         * al.add(str.charAt(i));
-         * for (int j = 0; j < ss.length(); j++) {
-         * if (str.charAt(i) == ss.charAt(j)) {
-         * count++;
-         * }
-         * if (count == ss.length()) {
-         * if (min > al.size()) {
-         * min = al.size();
-         * alresult = new ArrayList<>(al);
-         * }
-         * count = 0;
-         * al.clear();
-         * }
-         * }
-         * }
-         * System.out.println(alresult);
-         */
     }
 }
