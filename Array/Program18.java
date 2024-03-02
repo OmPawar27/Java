de0a3ee48dd2d13d6192fe4a736b1610022c6cb0
Om Pawar <ompawar4242@gmail.com>
2024-03-02 18:37:30 +0530

    initial commit

---
+++ Array/Program18.java
@@ -0,0 +1,34 @@
+// Q18. Find the shortest subArray containing both min and max element
+
+public class Program18{
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,1,3,4,6,4,6,5};
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > max)
+                max = arr[i];
+
+            if(arr[i] < min)
+                min = arr[i];
+        }
+
+        int count = 0;
+        int minCount = Integer.MAX_VALUE;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == min){
+                for(int j = i+1; j<arr.length; j++){
+                    if(arr[j] == max){
+                        count = j-i+1;
+                        if(count<minCount){
+                            minCount = count;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("Shortest subarray is:" + minCount); 
+    }
+}
