6a6e2607286995f84de6fc15ef84cdc83b3a0b77
Om Pawar <ompawar4242@gmail.com>
2024-03-03 17:29:35 +0530

    inital commit

---
+++ Array/Program22.java
@@ -0,0 +1,24 @@
+/*
+ * Q.22 Given an array of n numbers. The task is to find the first equlibrium
+ * point in the array and return the index
+ */
+
+ public class Program22 {
+    public static void main(String[] args) {
+        
+        int arr[] = {1,3,5,2,2};
+        
+        for(int i = 0; i<arr.length; i++){
+            int leftSum = 0;
+            int rightSum = 0;
+            for(int j = 0; j<i; j++){
+                leftSum += arr[j];
+            }
+            for(int k = arr.length-1; k>i; k--){
+                rightSum += arr[k];
+            }
+            if(leftSum == rightSum)
+                System.out.println(i);
+        }
+    }
+}
