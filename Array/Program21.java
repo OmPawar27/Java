a2ed550b012de87c5a4a44da49e23f717bcced31
Om Pawar <ompawar4242@gmail.com>
2024-03-03 17:29:06 +0530

    inital commit

---
+++ Array/Program21.java
@@ -0,0 +1,29 @@
+/*
+ * Q21. Given and array of DISTINCT elements.
+ * Find the missing element in the array
+ */
+
+ public class Program21{
+    public static void main(String[] args) {
+        int arr[] = {1,4,3,5};
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        int sum1 = 0;
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > max)
+                max = arr[i];
+            if(arr[i] < min)
+                min = arr[i];
+            sum1 += arr[i];
+        }
+
+        int sum2 = 0;
+        for(int i = min; i<=max; i++){
+            sum2 += i;
+        }
+
+        System.out.println("The missing element is:" + (sum2-sum1));
+    }
+}
