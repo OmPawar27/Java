fe91cc1185460d2cd6d9c748ec804acde69a01fd
Om Pawar <ompawar4242@gmail.com>
2024-03-06 22:55:45 +0530

    inital commit

--- Collection/ArrayList/Program2.java
+++ Collection/ArrayList/Program2.java
@@ -1,22 +1,53 @@
-/*
- * Given and array of n integers.The problem is to find the sum of the 
- * elements of the contiguous subarray having smallest(min) sum.
- */
+// Some basic methods
+
+import java.util.*;
 
 public class Program2 {
     public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
         
-        int arr[] = {3,-4,2,-3,-1,7,3};
-        int min = Integer.MAX_VALUE;
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(10);
+        al.add(20);
+        al.add(30);
 
-        for(int i = 0; i<arr.length; i++){
-            int sum = 0;
-            for(int j = i; j<arr.length; j++){
-                sum += arr[j];
-                if(sum < min)
-                    min = sum;
-            }
-        }
-        System.out.println(min); // -4,2,-3,-1 = -6
+        // Print all List
+        System.out.println(al);
+
+        // Size of the list
+        // Return Type: int
+        System.out.println(al.size());
+
+        // return True if object is present in list
+        System.out.println(al.contains(20));
+
+        // indexOf(obj)
+        // returns first index of object if present
+        System.out.println(al.indexOf(30));
+
+        // lastIndexOf(obj)
+        System.out.println(al.lastIndexOf(30));
+
+        // E get(int)
+        System.out.println(al.get(3));
+
+        // E set(index,obj)
+        al.set(3, 40);
+        System.out.println(al);
+
+        ArrayList<Integer> al2 = new ArrayList<Integer>();
+        al2.add(60);
+        al2.add(70);
+        al2.add(80);
+
+        // addAll(collection)
+        al.addAll(al2);
+        System.out.println(al);
+
+        // remove(int)
+        System.out.println(al.remove(1));
+
     }
 }
