eb0ab6eb672fcc9fea103ff538631bcf7cbc9ccb
Om Pawar <ompawar4242@gmail.com>
2024-03-06 22:55:10 +0530

    inital commit

--- Collection/ArrayList/Program1.java
+++ Collection/ArrayList/Program1.java
@@ -1,34 +1,19 @@
-// Q. Find the shortest subArray containing both min and max element
+// We can store different objects("Data")
+// Size is not fixed it is dynamic
+// Duplicate objects can be stored
+// Insertion order is maintained
+
+import java.util.*;
 
 public class Program1{
     public static void main(String[] args) {
-        int arr[] = {1,2,3,1,3,4,6,4,6,5};
+        
+        ArrayList al = new ArrayList();
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
+        al.add(10);     
+        al.add("Om");
+        al.add("30");
 
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] > max)
-                max = arr[i];
-
-            if(arr[i] < min)
-                min = arr[i];
-        }
-
-        int count = 0;
-        int minCount = Integer.MAX_VALUE;
-        for(int i = 0; i<arr.length; i++){
-            if(arr[i] == min){
-                for(int j = i+1; j<arr.length; j++){
-                    if(arr[j] == max){
-                        count = j-i+1;
-                        if(count<minCount){
-                            minCount = count;
-                        }
-                    }
-                }
-            }
-        }
-        System.out.println("Shortest subarray is:" + minCount); 
+        System.out.println(al);
     }
 }
