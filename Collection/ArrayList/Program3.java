4c5df3a13930072d428b15115a714f09cbbeccfe
Om Pawar <ompawar4242@gmail.com>
2024-03-06 22:55:58 +0530

    inital commit

--- Collection/ArrayList/Program3.java
+++ Collection/ArrayList/Program3.java
@@ -1,35 +1,37 @@
-/*
- * Given an array of positive integers numbers and a positive integer
- * target(key), retrun the minimal length of a subarray whose sum is
- *  grater than or eqyal to targer. IF there is no such subarray retrun 0
- */
+// Cursors Types - Iterator, ListIterator, Enumerator, SplitIterator
+// Iterator works on - List, Set, Queue
+// ListIterator works on - List only
+
+import java.util.*;
 
 public class Program3 {
     public static void main(String[] args) {
-        int arr[] = {2,3,1,2,4,3};
-        int key = 7;
+        ArrayList<String> al = new ArrayList<String>();
 
-        int minCount = Integer.MAX_VALUE;
-        boolean flag = false;
+        al.add("Om");
+        al.add("Vaibhav");
+        al.add("Prajwal");
+        al.add("Vishal");
+        // System.out.println(al);
 
-        for(int i = 0; i<arr.length; i++){
-            int count = 0;
-            int sum = 0;
-            for(int j = i; j<arr.length; j++){
-                count++;
-                sum += arr[j];
-                if(sum == key){
-                    flag = true;
-                    if(minCount > count)
-                        minCount = count;
-                }
-                if(sum > key)
-                    break;
-            }
+        // Iterator goes only FORWARD
+        Iterator iterator = al.iterator();
+        System.out.println("Iterator:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
-        if(flag == true)
-            System.out.println(minCount);
-        else
-            System.out.println(0);
+
+        // ListIterator can traverse on both direction(forward,backward)
+        ListIterator lisIterator = al.listIterator();
+        System.out.println("ListIterator(FORWARD):");
+        while (lisIterator.hasNext()) {
+            System.out.println(lisIterator.next());
+        }
+
+        System.out.println("ListIterator(BACKWARD):");
+        while (lisIterator.hasPrevious()) {
+            System.out.println(lisIterator.previous());
+        }
+
     }
 }
